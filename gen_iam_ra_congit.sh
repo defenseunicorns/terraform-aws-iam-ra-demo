fde05b9557d7d2372751d3911f217cec2cc27df6
John Trapnell <johntrapnell@defenseunicorns.com>
2024-02-06 16:35:35 -0700

    Switch to pkcs15-tool as being more reliable red CAC

--- gen_iam_ra_congit.sh
+++ gen_iam_ra_congit.sh
@@ -9,13 +9,15 @@ fi
 # Where the location of the test NPE cert  amd private key are located
 cert_dir="/Users/johntrapnell/settings/Certificates"
 
-cert_txt=$(pkcs11-tool --list-objects --type cert | grep -B 1 -A 3 "Certificate for PIV Authentication" 2>/dev/null)
+#cert_txt=$(pkcs11-tool --list-objects --type cert | grep -B 1 -A 3 "Certificate for PIV Authentication" 2>/dev/null)
+cert_txt=$(pkcs15-tool --list-certificates | grep -B 1 -A 5 "Certificate for PIV Authentication")
 cert_id=$(echo "${cert_txt}" | awk '/ID:/ {print $2}' 2>/dev/null)
 echo "Cert ID: ${cert_id}"
 serial=$(echo "${cert_txt}" | awk '/serial:/ {print $2}' 2>/dev/null)
 echo "Cert Serial: ${serial}"
 sleep 5
-issuer=$(pkcs11-tool --read-object --type cert --id "${cert_id}" | openssl x509 -inform DER  -text -noout -issuer | grep 'Issuer:' 2>/dev/null)
+#issuer=$(pkcs11-tool --read-object --type cert --id "${cert_id}" | openssl x509 -inform DER  -text -noout -issuer | grep 'Issuer:' 2>/dev/null)
+issuer=$(pkcs15-tool --read-certificate 01 | openssl x509 -text -noout | grep 'Issuer:' 2>/dev/null)
 #issuer="        Issuer: C=US, O=U.S. Government, OU=DoD, OU=PKI, CN=DOD ID CA-65"
 echo "Issuer: ${issuer}"
 cert_name=$(echo "${issuer}" | awk -F ', ' '{for(i=1; i<=NF; i++) if ($i ~ /^CN=/) print substr($i, 4)}' | awk '{ gsub(/[ -]/, "_"); print }' 2>/dev/null)
